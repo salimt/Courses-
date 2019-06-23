5a74d44f1713313d4a02c6461847db73519dc481
salimt <30296492+salimt@users.noreply.github.com>
2019-06-23 16:14:15 +0300

    Update recipe.rb

--- Johns Hopkins University - Ruby on Rails Web Development Specialization/Johns Hopkins University - Ruby on Rails/project/recipefinder/app/models/recipe.rb
+++ Johns Hopkins University - Ruby on Rails Web Development Specialization/Johns Hopkins University - Ruby on Rails/project/recipefinder/app/models/recipe.rb
@@ -1,27 +1,9 @@
-# class Recipe < ActiveRecord::Base
-
-# require 'httparty'
-# class Recipe
-# 	include HTTParty
-# 	base_uri 'http://food2fork.com/api'
-# 	ENV["FOOD2FORK_KEY"] = 'enter your key'
-# 	default_params key: ENV["FOOD2FORK_KEY"]
-# 	format :json
-
-#     def self.for(keyword)
-#     	get("/search", query: {q: keyword})["recipes"]
-#     end
-
-# end
-
-# end
-
 require 'httparty'
 
 class Recipe
 	include HTTParty
 	default_options.update(verify: false)
-	ENV["FOOD2FORK_KEY"] = '87a11451bcb03350180ea769efe8dc39'
+	ENV["FOOD2FORK_KEY"] = 'enter your key'
 	default_params key: ENV["FOOD2FORK_KEY"]
 	hostport = ENV['FOOD2FORK_SERVER_AND_PORT'] || 'www.food2fork.com'
 	base_uri "http://#{hostport}/api"
